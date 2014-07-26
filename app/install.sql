d1b0cbc5d58b1a091ea7dc1ef13eeb11a71fc080
Michael Caldera <michcald@gmail.com>
2014-07-27 00:42:58 +0100

    Using InnoDb and foreign keys

--- app/install.sql
+++ app/install.sql
@@ -8,10 +8,11 @@ CREATE TABLE IF NOT EXISTS meta_repository (
     `children` TEXT NULL,
     UNIQUE(`name`),
     PRIMARY KEY (`id`) 
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS meta_repository_field (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `repository_id` INTEGER NOT NULL,
     `name` VARCHAR(255) NOT NULL,
     `label` VARCHAR(255) NOT NULL,
     `description` TEXT NULL,
@@ -20,15 +21,22 @@ CREATE TABLE IF NOT EXISTS meta_repository_field (
     `sortable` TINYINT NOT NULL DEFAULT 0,
     `main` TINYINT NOT NULL DEFAULT 0,
     `list` TINYINT NOT NULL DEFAULT 0,
-    `type_id` INTEGER NOT NULL,
-    PRIMARY KEY (`id`)
-);
+    `type` INTEGER NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`repository_id`) REFERENCES `meta_repository`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS meta_app (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(255) NOT NULL,
     `description` TEXT NULL,
-    `password` VARCHAR(255) NOT NULL,
-    UNIQUE(`name`),
+    `public_key` VARCHAR(64) NOT NULL,
+    `private_key` VARCHAR(64) NOT NULL,
+    UNIQUE(`public_key`),
+    UNIQUE(`private_key`),
     PRIMARY KEY (`id`) 
-);
+) ENGINE=InnoDB;
+
+/* insert default application */
+
+INSERT INTO meta_app (`name`,`description`,`public_key`,`private_key`) VALUES ("dummy_app","Default application","829c3804401b0727f70f73d4415e162400cbe57b","f64133af6818761d95c8230953e5c9ddee1d0cf3");
