4a43657b4e53a932dd4088743595def3a90df55e
Michael Caldera <michcald@gmail.com>
2014-09-20 12:23:03 +0100

    Repository update interface

--- src/Michcald/Dummy/App/Controller/Administrable/RepositoryController.php
+++ src/Michcald/Dummy/App/Controller/Administrable/RepositoryController.php
@@ -19,8 +19,6 @@ class RepositoryController extends Crud implements Administrable
 
     public function createAction()
     {
-        // @TODO verify if the app is admin flagged
-
         $form = new \Michcald\Dummy\App\Form\Model\Repository();
 
         $form->setValues($this->getRequest()->getData());
@@ -29,7 +27,7 @@ class RepositoryController extends Crud implements Administrable
 
             $values = $form->getValues();
 
-            // @TODO verify if already exists
+            // verify if already exists
             $query = new \Michcald\Dummy\Dao\Query();
             $query->addWhere('name', $values['name']);
             $result = $this->dao->findOne($query);
@@ -83,12 +81,74 @@ class RepositoryController extends Crud implements Administrable
 
     public function updateAction($id)
     {
-        // @TODO verify if the app is admin flagged
+        $query = new \Michcald\Dummy\Dao\Query();
+        $query->addWhere('id', $id);
+
+        $repository = $this->dao->findOne($query);
+
+        if (!$repository) {
+            return new \Michcald\Dummy\Response\Json\NotFound('Repository not found: ' . $id);
+        }
+
+        $form = new \Michcald\Dummy\App\Form\Model\Repository();
+
+        $form->setValues($this->getRequest()->getData());
+
+        if ($form->isValid()) {
+
+            $values = $form->getValues();
+
+            $updatedRepo = $this->dao->create($values);
+
+            $updatedRepo->setId($repository->getId());
+
+            if ($repository->getName() != $updatedRepo->getName()) {
+                $response = new \Michcald\Dummy\Response\Json();
+                $response->setStatusCode(409) // conflict
+                    ->setContent(array(
+                        'error' => array(
+                            'status_code' => 409,
+                            'message' => 'The repository name cannot be changed'
+                        )
+                    ));
+                return $response;
+            }
+
+            $this->dao->persist($updatedRepo);
+
+            $response = new \Michcald\Dummy\Response\Json();
+            $response->setStatusCode(204)
+                ->setContent($repository->getId());
+
+            return $response;
+
+        } else {
+
+            $values = $form->getValues();
+
+            $formErrors = array();
+            foreach ($form->getElements() as $element) {
+                $formErrors[$element->getName()] = array(
+                    'value' => $values[$element->getName()],
+                    'errors' => $element->getErrorMessages()
+                );
+            }
+
+            $response = new \Michcald\Dummy\Response\Json();
+            $response->setStatusCode(400)
+                ->setContent(array(
+                    'error' => array(
+                        'status_code' => 400,
+                        'message' => 'Data not valid',
+                        'form' => $formErrors
+                    )
+                ));
+            return $response;
+        }
     }
 
     public function deleteAction($id)
     {
-        // @TODO verify if the app is admin flagged
         $query = new \Michcald\Dummy\Dao\Query();
         $query->addWhere('id', $id);
 
